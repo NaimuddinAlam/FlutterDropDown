1935a19595354bc1dea5c38b1d70ba7d60df7207
Admin <naimuddinalam12@gmail.com>
2022-06-07 05:52:28 +0530

    Flutter DropDown

---
+++ android/app/src/main/kotlin/com/example/dropdwown_radio_checkbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dropdwown_radio_checkbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+ // var selected="One";
+  var selected=1;
+//  final dropdownlist=<String>["One","Two","Three","Four"];
+
+  final dropdownlist=<int>[1,2,3,4];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DropDown"),
+      ),
+
+
+     body: Center(child: DropdownButton(
+       value: selected.toInt(),
+       items: dropdownlist
+           .map((e) => DropdownMenuItem(value: e, child: Text(e.toString())))
+           .toList(),
+       onChanged: (int? value) {
+         if(value!=null)
+           {
+             setState(() {
+               selected=value;
+             });
+
+
+           }
+
+       },)),
+
+    );
+  }
+}
+
+
+
